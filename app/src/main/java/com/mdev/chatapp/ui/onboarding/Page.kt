5fc560a9cb615748991dcb8aab75cd8654598727
John Hooin <johnnaeder6537@gmail.com>
2024-06-09 12:39:16 +0700

    add showcase

--- app/src/main/java/com/mdev/chatapp/ui/onboarding/Page.kt
+++ app/src/main/java/com/mdev/chatapp/ui/onboarding/Page.kt
@@ -12,17 +12,17 @@ data class Page(
 val pages = listOf(
     Page(
         title = "App chat DRP.",
-        description = "Được phát triển bởi 3 chú be vjp zo.",
+        description = "DRP chat bot by John hooin.",
         image = R.drawable.onboarding1
     ),
     Page(
-        title = "Mục đích",
-        description = "Cung cấp chat bot hỗ trợ tư vấn ăn uống.",
+        title = "Aim",
+        description = "An approach to providing a personalized list of",
         image = R.drawable.onboarding2
     ),
     Page(
         title = "Enzuyzepzaivl",
-        description = "Hết ý tưởng ghi rồi",
+        description = "suitable dishes for each individual through a friendly",
         image = R.drawable.onboarding3
     )
 )
